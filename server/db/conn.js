ac87492a09d3994543bbb715695c9bd0af6c43cd
Reeyan Khimani <Khimanireeyan@gmail.com>
2023-01-19 14:29:15 -0500

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,8 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ReeyanK:Reeyan18@cluster0.c4dg0un.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
-};
