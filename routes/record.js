78b9778966e186b277f448a82807dbe485cd6c5c
Reeyan Khimani <reeyankhimani@Reeyans-MacBook-Air.local>
2023-03-09 14:45:21 -0500

    init for personalWebsite

--- routes/record.js
+++ routes/record.js
@@ -14,9 +14,9 @@ const ObjectId = require("mongodb").ObjectId;
  
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
- let db_connect = dbo.getDb("employees");
+ let db_connect = dbo.getDb("Personal");
  db_connect
-   .collection("records")
+   .collection("users")
    .find({})
    .toArray(function (err, result) {
      if (err) throw err;
@@ -29,13 +29,13 @@ recordRoutes.route("/record/:id").get(function (req, res) {
  let db_connect = dbo.getDb();
  let myquery = { _id: ObjectId(req.params.id) };
  db_connect
-   .collection("records")
+   .collection("users")
    .findOne(myquery, function (err, result) {
      if (err) throw err;
      res.json(result);
    });
 });
- 
+
 // This section will help you create a new record.
 recordRoutes.route("/record/add").post(function (req, response) {
  let db_connect = dbo.getDb();
@@ -44,7 +44,7 @@ recordRoutes.route("/record/add").post(function (req, response) {
    position: req.body.position,
    level: req.body.level,
  };
- db_connect.collection("records").insertOne(myobj, function (err, res) {
+ db_connect.collection("users").insertOne(myobj, function (err, res) {
    if (err) throw err;
    response.json(res);
  });
@@ -62,7 +62,7 @@ recordRoutes.route("/update/:id").post(function (req, response) {
    },
  };
  db_connect
-   .collection("records")
+   .collection("users")
    .updateOne(myquery, newvalues, function (err, res) {
      if (err) throw err;
      console.log("1 document updated");
@@ -74,7 +74,7 @@ recordRoutes.route("/update/:id").post(function (req, response) {
 recordRoutes.route("/:id").delete((req, response) => {
  let db_connect = dbo.getDb();
  let myquery = { _id: ObjectId(req.params.id) };
- db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+ db_connect.collection("users").deleteOne(myquery, function (err, obj) {
    if (err) throw err;
    console.log("1 document deleted");
    response.json(obj);
